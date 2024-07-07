bafdb9b097990acc6042d07e3384a8ca0b6a09a1
Tom Labiausse <tom01lab@gmail.com>
2024-07-07 19:37:18 +0200

    v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-// #![allow(unused)]
-
 use std::env;
 use std::fs;
+use std::io::{self, Write};
+use rand::Rng;
 
 const DEFAULT_DICT_PATH: &str = "data/dictionary.txt";
+const CLEAR_CONSOLE: &str = "\x1B[2J\x1B[1;1H";
 
 pub struct Config {
     pub dict_path: String,
@@ -49,5 +50,88 @@ impl Config {
         }
 
         Ok(Config { dict_path, dict, ignore_case })
+    }
+
+}
+
+/// Initialize the game by sampling a word from the dictionary and splitting it into a vector of characters.
+fn init_game(config: &Config) -> (String, Vec<char>) {
+    let word_idx = rand::thread_rng().gen_range(0..config.dict.len());
+    let secret_word = config.dict[word_idx].clone();
+    let word_characters: Vec<char> = secret_word.chars().collect();
+    (secret_word, word_characters)
+}
+
+/// Game logic
+pub fn game(config: &Config) -> Result<(), String>{
+
+    // sample a word according to the config
+    let (secret_word, word_characters) = init_game(config);
+
+    // initialize state of the game variables
+    let mut state: Vec<char> = vec!['_'; word_characters.len()];
+    let mut to_guess = word_characters.len(); // remaining number of characters to guess
+    let mut lives = 6;
+    let mut tried_letters: Vec<char> = vec![]; // unique set of letters that were already tried
+
+    // main game loop
+    loop {
+
+        print!("{CLEAR_CONSOLE}");
+
+        // display the state of the game
+        let str_state: String = state.iter().collect();
+        println!("You have {lives} lives left and state of the game is: {str_state}");
+        let str_tried_letters: String = 
+            tried_letters.iter().map(|c| c.to_string().to_uppercase()).collect::<Vec<String>>().join("");
+        println!("\nLetters already tried: {str_tried_letters}");
+        
+        // prompt the user for a guess
+        print!("\nPlease input your guess: ");
+        io::stdout().flush().unwrap(); // flush the buffer to display the prompt
+
+        // read the input from the user
+        let mut str_input = String::new();
+        io::stdin().read_line(&mut str_input).expect("Failed to read line");
+        let input = str_input.trim().to_string().to_lowercase().chars().collect::<Vec<char>>();
+        let letter: char;
+        if input.len() == 1 {
+            letter = char::from(input[0]);
+        } else {
+            continue;
+        }
+        
+        // check if the letter was already tried
+        if tried_letters.contains(&letter) {
+            continue;
+        } else {
+            tried_letters.push(letter);
+        }
+        
+        // update the state of the game
+        if word_characters.contains(&letter) {
+            for (i, _) in word_characters.iter().enumerate() {
+                if word_characters[i] == letter {
+                    state[i] = letter;
+                    to_guess -= 1;
+                }
+            }
+        } else {
+            lives -= 1;
+            // check for defeat
+            if lives == 0 {
+                println!("\nYou lost! The secret word was '{secret_word}'.");
+                return Ok(());
+            } else {
+                println!("Letter not found in the word.");
+            }   
+        }
+        
+        // check for victory
+        if to_guess == 0 {
+            println!("\nYou win! The secret word was '{secret_word}'.");
+            return Ok(());
+        }
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-#![allow(unused)]
-
-use std::path::Path;
 use std::process;
-use std::fs;
 use std::env;
-use std::io::{self, Write};
-use rand::Rng;
 
-use hangman_rust::Config;
-
-const CLEAR_SCREEN: &str = "\x1B[2J\x1B[1;1H";
-const ALIVE_CELL: &str = "\u{2588}\u{2588}";
-const DEAD_CELL: &str = "  ";
-
-// println!("\n{ALIVE_CELL}  {ALIVE_CELL}");
-// println!("  {ALIVE_CELL}  ");
-// println!("{ALIVE_CELL}  {ALIVE_CELL}");
+use hangman_rust::{Config, game};
 
 fn main() {
 
@@ -26,74 +12,9 @@ fn main() {
         process::exit(1);
     });
 
-    let word_idx = rand::thread_rng().gen_range(0..config.dict.len());
-
-    let secret_word = config.dict[word_idx].clone();
-
-    let word_characters: Vec<char> = secret_word.chars().collect();
-
-    let mut state: Vec<char> = vec!['_'; word_characters.len()];
-    let mut str_state: String = state.iter().collect();
-
-    let mut to_guess = word_characters.len();
-    let mut lives = 6;
-
-    loop {
-
-        print!("{CLEAR_SCREEN}");
-        
-        let str_state: String = state.iter().collect();
-        println!("You have {lives} lives left and state of the game is: {str_state}");
-        
-        print!("\nPlease input your guess: ");
-        io::stdout().flush().unwrap(); // flush the buffer to display the prompt
-
-        let mut letter = String::new();
-
-        io::stdin()
-            .read_line(&mut letter)
-            .expect("Failed to read line");
-        
-        // not WAI yet as it does not handle the case where the user types more than one letter
-        let letter = letter.trim().to_string().to_lowercase().chars().next().unwrap_or_else(|| {
-            eprintln!("Please type a letter.");
-            process::exit(1);
-        });
-
-        // println!("You guessed: {letter}");
-
-        if state.contains(&letter) {
-            println!("\nYou already guessed the letter {letter}");
-            continue;
-        }
-        
-        if word_characters.contains(&letter) {
-            for (i, _) in word_characters.iter().enumerate() {
-                if word_characters[i] == letter {
-                    state[i] = letter;
-                    to_guess -= 1;
-                }
-            }
-        } else {
-            lives -= 1;
-            if lives == 0 {
-                println!("\nYou lost! The secret word was '{secret_word}'.");
-                break;
-            } else {
-                println!("Letter not found in the word.");
-            }   
-        }
-        
-        if to_guess == 0 {
-            println!("\nYou win! The secret word was '{secret_word}'.");
-            break;
-        }
-
-    }
+    let _ = game(&config).unwrap_or_else(|err| {
+        eprintln!("ERROR when playing the game: {err}");
+        process::exit(1);
+    });
 
 }
-
-// TODO
-// * display the letters that were already tried
-// * don't take a live it it was already tried
-// * ask for a new output if the user types more than one character
